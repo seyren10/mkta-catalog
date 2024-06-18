a4df1c2d8ecb5c58b34faf5ec81ea89ad2539ee2
seyren10 <87021985+seyren10@users.noreply.github.com>
2024-06-18 16:02:38 +0800

    Added Chips

---
+++ resources/js/components/Chips/VChipGroup.vue
@@ -0,0 +1,57 @@
+<template>
+    <div
+        class="relative flex flex-wrap items-center gap-3 rounded-lg border p-3"
+    >
+        <TransitionGroup
+            enter-active-class="duration-300"
+            leave-active-class="duration-300"
+            enter-from-class="opacity-0"
+            leave-to-class="opacity-0"
+        >
+            <v-chip
+                v-for="(item, index) in items"
+                class="text-[.8rem]"
+                icon-size=".8"
+                :prepend-inner-icon="item.icon"
+                :key="isItemObject ? item.id : index"
+            >
+                <template #append-inner v-if="clearable">
+                    <v-icon
+                        name="md-close-round"
+                        @click="$emit('delete', item)"
+                        class="cursor-pointer"
+                    ></v-icon>
+                </template>
+
+                <template #prepend-inner v-if="isItemObject && item.icon">
+                    <v-icon :name="item.icon"></v-icon>
+                </template>
+
+                {{ isItemObject ? item.value : item }}</v-chip
+            >
+        </TransitionGroup>
+
+        <slot name="append" ref="append"></slot>
+    </div>
+</template>
+
+<script setup>
+import { computed, inject, ref } from "vue";
+
+const props = defineProps({
+    items: [Array],
+});
+defineEmits(["delete"]);
+
+const append = ref(null);
+const clearable = inject("clearable", false);
+
+//derived
+const isItemObject = computed(() => {
+    if (!props.items) return false;
+
+    return typeof props.items[0] === "object";
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ resources/js/components/Chips/VChipInput.vue
@@ -0,0 +1,96 @@
+<template>
+    <v-chip-group :items="collection" @delete="handleRemoveItem">
+        <template #append>
+            <div class="relative grow">
+                <input
+                    type="text"
+                    class="w-full outline-none"
+                    @keydown.enter="handleAddItem"
+                    v-model="input"
+                    @focus="isInputFocus = true"
+                    @blur="handleInputBlur"
+                    ref="el"
+                />
+                <!-- #region overlay -->
+                <div
+                    v-if="isInputFocus"
+                    class="absolute inset-x-0 top-[120%] z-[1000] overflow-hidden rounded-lg bg-white shadow-lg"
+                >
+                    <ul class="grid gap-2">
+                        <li
+                            v-for="item in searchSuggestions"
+                            @click="handleAddSuggestion(item)"
+                            class="cursor-pointer p-2 hover:bg-slate-200"
+                        >
+                            {{ item.value }}
+                        </li>
+                    </ul>
+                </div>
+                <!-- #endregion -->
+            </div>
+        </template>
+    </v-chip-group>
+</template>
+
+<script setup>
+import { computed, nextTick, provide, ref } from "vue";
+
+const props = defineProps({
+    clearable: Boolean,
+    items: Array,
+    appendable: Boolean,
+});
+
+const collection = ref([]);
+const input = ref("");
+const el = ref(null);
+const isInputFocus = ref(false);
+const excludedSuggestions = ref([]);
+
+//provide
+provide("clearable", props.clearable);
+
+//derives
+const excludedSuggestionsComputed = computed(() => {
+    if (!excludedSuggestions.value.length) return props.items;
+
+    return props.items.filter((e) => {
+        return !excludedSuggestions.value.find((f) => +f.id === +e.id);
+    });
+});
+
+const searchSuggestions = computed(() => {
+    if (input.value.trim() === "") return excludedSuggestionsComputed.value;
+
+    return excludedSuggestionsComputed.value.filter((e) => {
+        return e.value.toLowerCase().includes(input.value.toLowerCase());
+    });
+});
+
+//methods
+const handleAddItem = () => {
+    collection.value.push({ id: Math.random(), value: input.value });
+    input.value = "";
+};
+
+const handleRemoveItem = (item) => {
+    const removeItem = collection.value.findIndex((e) => +e.id === +item.id);
+    collection.value.splice(removeItem, 1);
+
+    excludedSuggestions.value = excludedSuggestions.value.filter(
+        (e) => +e.id !== +item.id,
+    );
+};
+
+const handleAddSuggestion = (item) => {
+    collection.value.push(item);
+    excludedSuggestions.value.push(item);
+};
+const handleInputBlur = () => {
+    setTimeout(() => {
+        isInputFocus.value = false;
+    }, 100);
+};
+</script>
+
+<style lang="scss" scoped></style>
