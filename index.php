56322f35ff30eb5424f79ec1d5b030358d253954
Kirito08231995 <johnrickyl@ymail.com>
2024-07-18 17:54:53 +0800

    added index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>MK Themed Attractions Phils</title>
+
+    @vite('resources/js/app.js')
+</head>
+
+<body class="text-slate-800 antialiased bg-stone-100 ">
+    <div id="overlay"></div>
+    <div id="app">
+    </div>
+</body>
+
+</html>
