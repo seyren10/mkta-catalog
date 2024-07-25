e456a8ac99f13504ed53d51b65d4adf6165f0614
seyren10 <87021985+seyren10@users.noreply.github.com>
2024-07-26 07:07:38 +0800

    changes VToaster parent timeout to children timeout , in able to set timeout for each VToaster

--- resources/js/composables/useToaster.js
+++ resources/js/composables/useToaster.js
@@ -9,29 +9,24 @@ export const useToaster = (props) => {
 
     const intervalId = ref(null);
 
-    const lastTime = ref(toaster.value.timeout);
-
     function addToast(options) {
         options.id = Math.floor(Math.random() * 10_000).toString();
 
         toaster.value.children.push(options);
 
-        startQueuing();
+        if (!intervalId.value) startQueuing();
     }
 
     function startQueuing() {
-        clearInterval(intervalId.value);
-        if (!toaster.value.children.length) return;
+        if (!toaster.value.children.length) {
+            intervalId.value = null;
+            return;
+        }
 
-        intervalId.value = setInterval(() => {
-            lastTime.value -= 1000;
-            console.log(lastTime.value);
-            if (lastTime.value <= 0) {
-                toaster.value.children.shift();
-                lastTime.value = toaster.value.timeout;
-                startQueuing();
-            }
-        }, 1000);
+        intervalId.value = setTimeout(() => {
+            toaster.value.children.shift();
+            startQueuing();
+        }, toaster.value.children[0].timeout || toaster.value.timeout);
     }
 
     return { toaster: toaster.value, addToast };
